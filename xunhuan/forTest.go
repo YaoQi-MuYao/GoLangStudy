1d719ce7364e8909da92773f5c00e5e9c0963621
MuYao <1253043972@qq.com>
2021-03-10 18:05:27 +0800

    GoLang 循环语句

---
+++ xunhuan/forTest.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	sum := 1
+
+	for i := 0; i <= 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
+
+	for1()
+	for2()
+}
+
+func for1() {
+
+	/* 第二种形式 */
+	sum := 1
+	for sum <= 10 {
+		sum += sum
+	}
+	fmt.Println(sum)
+
+	/* 第三种形式 */
+	sum = 1
+	for sum <= 10 {
+		sum += sum
+	}
+	fmt.Println(sum)
+}
+
+/*
+	foreach 循环结构
+*/
+func for2() {
+
+	strings := []string{"muyao", "wenruo"}
+	for i, s := range strings {
+		fmt.Println(i, s)
+	}
+
+	numbers := [6]int{1, 2, 3, 4}
+	for i, x := range numbers {
+		fmt.Printf("第 %d 位 x 的值 = %d\n", i, x)
+	}
+}
