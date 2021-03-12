604cc9b27d634d4e90d67643e5c60cdb2a135a4c
MuYao <1253043972@qq.com>
2021-03-12 11:32:58 +0800

    GoLang map

--- map/mapTest1.go
+++ map/mapTest1.go
@@ -1,6 +1,27 @@
 package main
 
+import "fmt"
+
 /* delete 函数 */
 func main() {
 
+	/* 创建map */
+	countryCapitalMap := map[string]string{"France": "Paris", "Italy": "Rome", "Japan": "Tokyo", "India": "New delhi"}
+
+	fmt.Println("原始地图")
+
+	/* 打印地图 */
+	for country := range countryCapitalMap {
+		fmt.Println(country, "首都是", countryCapitalMap[country])
+	}
+
+	/* 删除元素 */
+	delete(countryCapitalMap, "France")
+	fmt.Println("法国首都已经删除")
+	fmt.Println("删除元素后的地图")
+
+	/* 打印地图 */
+	for country := range countryCapitalMap {
+		fmt.Println(country, "首都是", countryCapitalMap[country])
+	}
 }
